6f279f094baac9d9044ea0597b2eeba2d75891a1
DarekParodia <darekparodia@gmail.com>
2024-05-23 01:45:54 +0200

    chore: Update database structure and data for categories and tags

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2024 at 10:54 PM
+-- Generation Time: Maj 23, 2024 at 01:45 AM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -29,9 +29,42 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `torrent` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(2, 'Games'),
+(4, 'Music'),
+(5, 'Others'),
+(3, 'Software'),
+(1, 'Videos');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tag_connector`
+--
+
+CREATE TABLE `tag_connector` (
+  `id` int(11) NOT NULL,
+  `torrent_id` int(11) DEFAULT NULL,
+  `tag_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -42,13 +75,14 @@ CREATE TABLE `categories` (
 CREATE TABLE `torrents` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `magnet` int(11) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `description` varchar(1500) NOT NULL,
+  `magnet` varchar(1000) NOT NULL,
   `size` int(11) NOT NULL,
   `seeders` int(11) NOT NULL,
   `leenchers` int(11) NOT NULL,
-  `upload_date` date NOT NULL
+  `upload_date` date NOT NULL,
+  `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -65,16 +99,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'darekparodia@gmail', '$2y$10$0/gm.qMQUBgFLOauMjZIfOLLLfP4YudBfZzbNBW8lsPy6AxwX/u4m', 'darekparodia@gmail.com'),
-(2, 'darekparodia@gmail', '123123', 'darekparodia@gmail.com'),
-(3, '<b>Darek</b>', 'awf', 'awfawf@awdf.ad'),
-(4, 'qweqweqwe', '$2y$10$7n/7KKo2s57OcRNWspurQeYbuyGd38SygttJn61u7VMQVwFYaJNqq', 'awdawd@awdawd.ad');
-
---
 -- Indeksy dla zrzutów tabel
 --
 
@@ -83,8 +107,22 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `Categories_fk1` (`torrent`);
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indeksy dla tabeli `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indeksy dla tabeli `tag_connector`
+--
+ALTER TABLE `tag_connector`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `torrent_id` (`torrent_id`),
+  ADD KEY `tag_id` (`tag_id`);
 
 --
 -- Indeksy dla tabeli `torrents`
@@ -92,7 +130,8 @@ ALTER TABLE `categories`
 ALTER TABLE `torrents`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `torrents_fk1` (`user_id`);
+  ADD KEY `torrents_fk1` (`user_id`),
+  ADD KEY `fk_torrent_category` (`category_id`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -110,34 +149,48 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tag_connector`
+--
+ALTER TABLE `tag_connector`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `torrents`
 --
 ALTER TABLE `torrents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `categories`
+-- Constraints for table `tag_connector`
 --
-ALTER TABLE `categories`
-  ADD CONSTRAINT `Categories_fk1` FOREIGN KEY (`torrent`) REFERENCES `torrents` (`id`);
+ALTER TABLE `tag_connector`
+  ADD CONSTRAINT `tag_connector_ibfk_1` FOREIGN KEY (`torrent_id`) REFERENCES `torrents` (`id`),
+  ADD CONSTRAINT `tag_connector_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`);
 
 --
 -- Constraints for table `torrents`
 --
 ALTER TABLE `torrents`
+  ADD CONSTRAINT `fk_torrent_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
   ADD CONSTRAINT `torrents_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
